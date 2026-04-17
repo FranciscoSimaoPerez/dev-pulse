7a73b551c4899e3f106a6924b458aae752bfa62f
Francisco Perez <francisco_simao_perez@hotmail.com>
2026-04-17 12:08:17 +0100

    chore: update dependencies and add new packages for authentication and formatting
    - Added @auth/prisma-adapter, @prisma/client, @types/bcryptjs, bcryptjs, next-auth, and prisma for enhanced authentication and database support.
    - Updated eslint and added @typescript-eslint/parser for improved linting.
    - Introduced prettier and prettier-eslint for code formatting.

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,8 @@
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+
+const handler = NextAuth({
+    providers: [GitHub]
+})
+
+export { handler as GET, handler as POST }
